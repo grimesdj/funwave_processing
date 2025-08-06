4293ebc6717f63dde636eaeec178de281d5e7d96
grimesdj <grimesdj@uncw.edu>
2025-08-06 11:24:21 -0400

    adding code for estimating alongshore fft

---
+++ code/alongshore_fft_estimate.m
@@ -0,0 +1,23 @@
+function [out,ky] = alongshore_fft_estimate(info,in);
+%
+% USAGE: [out,ky] = alongshore_fft_estimate(info,in);
+%
+% fft of "in" [NxM] is over N-rows, M-columns are not altered
+dy = info.dy;
+Ny = info.Ny-1;
+if iseven(Ny)
+    inyq = 1;% don't double the nyquist frequency
+    stop = Ny/2+1;
+else
+    inyq = 0;%
+    stop = (Ny+1)/2;
+end
+ky = [0:stop-1]'/(dy*Ny);
+%
+%
+sz  = size(in);
+in  = reshape(in,Ny,prod(sz(2:end)));
+out = detrend(in);
+out = fft(out);
+out = out(1:stop,:);
+out = reshape(out,[length(ky), sz(2:end)]);
