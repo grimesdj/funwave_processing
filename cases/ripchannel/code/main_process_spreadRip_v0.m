9bd36519efaa419264f1582dfb7ed7e817944943
grimesdj <grimesdj@uncw.edu>
2026-02-09 13:02:35 -0500

    processing the new version of spreadRip

--- cases/ripchannel/code/main_process_spreadRip_v0.m
+++ cases/ripchannel/code/main_process_spreadRip_v0.m
@@ -1,17 +1,13 @@
-%% error in input files (zero-spread are non-binary i/o) corrupted first run: barRip0_h05t10s00d00
-%% processing s~=0 here...
-
-
 % code to be launched on cms-hpc "cuttlefish"
 addpath(genpath('/storage/cms/grimesdj_lab/grimesdj/git/funwave/'))
 rootDIR = '/scratch/grimesdj/ripchannel/';
 % code to be launched on cms-hpc "cuttlefish"
 % 0) requires the input bathymetry name as top-dir
 runBATHYlist = {'spreadRip'};
-reproc  = 1;% 1=reprocess ascii to mat
+reproc  = 0;% 1=reprocess ascii to mat
 rmfiles = 0;% 1=remove original ascii files when finished
-recalc  = 1;% 1=recalculate run statistics
-plotter = 1;% 1=plot run statistics
+recalc  = 0;% 1=recalculate run statistics
+plotter = 1;% 1=re-plot statistics
 %
 %
 for ii=1:length(runBATHYlist)
@@ -35,8 +31,7 @@ load([matDIR,filesep,'runs_to_process.mat'])
 %
 % loop over run_dirs
 Ndirs  = length(run_dirs);
-disp('ONLY PROCESSING NON-ZERO SPREAD CASES [2:5,7:10,12:15]')
-for jj = [9:10,12:15]%[2:5,7:10,12:15]
+for jj = 1:Ndirs
 % 1) get current run subdirectory to process:
 runID    = run_dirs{jj};
 fprintf('\n processing: %s %s \n', runBATHY,runID)    
@@ -49,7 +44,7 @@ end
 info  = load([infoFile(1).folder,filesep,infoFile(1).name]);
 %
 %
-if reproc & jj>9
+if reproc 
     info = prep_info_structure(info);
     %
     % run specific output grid info:
@@ -62,19 +57,17 @@ if reproc & jj>9
     NwaveAvg = floor((info.TOTAL_TIME-info.STEADY_TIME)/info.T_INTV_wavg);
     dt_lp = info.T_INTV_wavg*ones(NwaveAvg,1);
     t_lp = [1:NwaveAvg]*dt_lp(1);
-    % binary output files... faster archiving
-    isBINARY = 1;
-    fLog = convert_funwave_output_to_NetCDF(info.rootOut,[info.rootMat,info.rootName],vars,t_lp,dt_lp,info.dx,spanx,rngx,info.dy,spany,rngy,rmfiles,300,[-inf inf],isBINARY,info.Nx-1,info.Ny-1);
+    fLog = convert_funwave_output_to_NetCDF(info.rootOut,[info.rootMat,info.rootName],vars,t_lp,dt_lp,info.dx,spanx,rngx,info.dy,spany,rngy,rmfiles,300);
     % construct time vector for Radiation Stress variables
-    vars = {'BrkDissX','BrkDissY','DxSxx','DxSxy','DxUUH','DxUVH','DySxy','DySyy','DyUVH','DyVVH','FRCX','FRCY','PgrdX','PgrdY','Sxx','Syy','Sxy','umean','vmean','etamean','Hsig'};
+    vars = {'BrkDissX','BrkDissY','DxSxx','DxSxy','DxUUH','DxUVH','DySxy','DySyy','DyUVH','DyVVH','FRCX','FRCY','PgrdX','PgrdY','Sxx','Syy','Sxy','umean','vmean','etamean'};
     Nbrk = floor((info.TOTAL_TIME-info.STEADY_TIME)/info.T_INTV_mean);
     dt_lp = info.T_INTV_mean*ones(Nbrk,1);
     t_lp = [1:Nbrk]*dt_lp(1);
-    fLog = convert_funwave_output_to_single_NetCDF(info.rootOut,[info.rootMat,info.rootName,'MomentumTerms'],vars,t_lp,dt_lp,info.dx,spanx,rngx,info.dy,spany,rngy,rmfiles,300,[-inf inf],isBINARY,info.Nx-1,info.Ny-1);
+    fLog = convert_funwave_output_to_single_NetCDF(info.rootOut,[info.rootMat,info.rootName,'MomentumTerms'],vars,t_lp,dt_lp,info.dx,spanx,rngx,info.dy,spany,rngy,rmfiles,300);
 end
 %
 %
-if recalc & jj>9
+if recalc 
     info = estimate_FUNWAVE_run_statistics_WaveAvgVelocity(info);
 end
 %
@@ -84,8 +77,56 @@ if plotter
 fout_stats    = plot_FUNWAVE_run_WaveAvgVelocity_statistics(info)
 fout_momentum = plot_FUNWAVE_run_momentum(info)
 fout_momentum_offline = plot_FUNWAVE_offline_momentum_budget(info)
-close all
 end
 end
 end
 
+exit
+
+% check for instantaneous files and proceed if they exist...
+%% fast time wave files
+for jj = 1:Ndirs
+% 1) get current run subdirectory to process:
+runID    = run_dirs{jj};
+fprintf('\n processing fast-time: %s - %s \n', runBATHY,runID)    
+% 2) get the archived info structure:
+infoFile = dir([matDIR,filesep,'*','info','*',runID,'.mat']);
+if length(infoFile)>1
+    fprintf('\tmultiple run-info files for:\t %s\n',runID)
+    fprintf('\tusing filename:\t\t\t %s\n',infoFile(1).name);
+end
+info  = load([infoFile(1).folder,filesep,infoFile(1).name]);
+%
+%
+if reproc
+    spanx = 1;
+    spany = 1;
+    rngx  = [1 info.subDomain(4)+1];% rngx  = [1 info.Lx/info.dx];
+    rngy  = [1 info.subDomain(2)+1];% rngy  = [1 info.Ly/info.dy];
+    % 3) load the output times and dts
+    info.timeFile = [info.rootSim,'time_dt.out'];
+    if ~exist(info.timeFile,'file')
+        fprintf('No fast-time output for: %s-%s',runBATHY,runID)
+        continue
+    end
+    Tdt = load(info.timeFile);
+    t0   = Tdt(:,1);
+    dt0  = gradient(t0);
+    dT0  = Tdt(:,2); clear Tdt
+    info.dt = mean(dt0);
+% $$$     save(info.fileName,'-struct','info')
+    %
+    % 4) convert the funwave output ascii files to .mat
+    vars = {'eta','mask','BrkSrcX','BrkSrcY'};
+    fLog = convert_funwave_output_to_NetCDF(info.rootOut,[info.rootMat,info.rootName],vars,t0,dT0,info.dx,spanx,rngx,info.dy,spany,rngy,rmfiles,300);
+    save(info.fileName,'-struct','info')
+end
+%
+if recalc 
+    info = estimate_FUNWAVE_run_statistics_Waves(info);
+end
+%
+%
+% plot the run statistics
+fout_stats    = plot_FUNWAVE_run_Wave_statistics(info)
+end
